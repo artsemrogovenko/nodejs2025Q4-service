d63e50a589b02884bf842941bc942f7ddb9da2fb
artsemrogovenko <taftoos2@gmail.com>
2025-11-29 19:10:33 +0300

    chore: rollback to initial configs

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import type { StatusCodes } from 'http-status-codes';
-
 export interface IUser {
   id: string; // uuid v4
   login: string;
@@ -46,12 +44,3 @@ export interface FavoritesResponse {
   albums: IAlbum[];
   tracks: ITrack[];
 }
-
-export type Codes =
-  | StatusCodes.OK
-  | StatusCodes.CREATED
-  | StatusCodes.NO_CONTENT
-  | StatusCodes.BAD_REQUEST
-  | StatusCodes.FORBIDDEN
-  | StatusCodes.NOT_FOUND
-  | StatusCodes.UNPROCESSABLE_ENTITY;
